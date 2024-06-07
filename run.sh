5650a867ed422cd7463226a4387dabd400581881
Jakub Świtała <130761957+Koperr@users.noreply.github.com>
2024-06-07 22:34:39 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 cd build
+make
+cd ..
+
+cd bin/Linux/Debug
 # directory, where the executable file is located, may differ if compiling in Release mode 
 # or Debug mode, also depends on OS
 ./OpenGL-Template
